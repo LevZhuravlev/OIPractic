da6f689ba8a479ad03ad82aec42423ae8ab55b24
LevZhuravlev <leoalexShuravl@gmail.com>
2021-06-22 19:46:20 +0300

    Initial Commit

---
+++ practic1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practic1
+//
+//  Created by Zhuravlev Lev on 22.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
